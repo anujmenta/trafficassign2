8b5c98641641586443bf556b4fb83724d9a0cff7
Anuj Menta <anujmenta@gmail.com>
2019-10-31 16:45:27 -0400

    [debug] printing btsize

--- model.py
+++ model.py
@@ -71,7 +71,8 @@ class Net(nn.Module):
         # Perform the usual forward pass
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-	x = x.view(-1, 3200)
+        print(bt_size)
+        x = x.view(-1, 3200)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
