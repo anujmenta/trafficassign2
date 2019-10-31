039c7373f59ca84d01f601ecc4900d4b271d297d
Anuj Menta <anujmenta@gmail.com>
2019-10-31 17:28:37 -0400

    pull back to old params

--- model.py
+++ model.py
@@ -27,15 +27,13 @@ class Net(nn.Module):
         super(Net, self).__init__()
         self.conv1 = nn.Conv2d(3, 100, kernel_size=5)
         self.batchnorm1 = nn.BatchNorm2d(100)
-        self.conv2 = nn.Conv2d(100, 200, kernel_size=5)
+        self.conv2 = nn.Conv2d(100, 200, kernel_size=3)
         self.batchnorm2 = nn.BatchNorm2d(200)
-        self.conv3 = nn.Conv2d(200, 400, kernel_size=3)
-        self.batchnorm3 = nn.BatchNorm2d(400)
-        self.conv4 = nn.Conv2d(400, 750, kernel_size=3)
-        self.batchnorm4 = nn.BatchNorm2d(750)
+        self.conv3 = nn.Conv2d(200, 250, kernel_size=3)
+        self.batchnorm3 = nn.BatchNorm2d(250)
         self.dropout = nn.Dropout2d()
-        self.fc1 = nn.Linear(1000*2*2, 500)
-        self.fc2 = nn.Linear(500, nclasses)
+        self.fc1 = nn.Linear(250*2*2, 150)
+        self.fc2 = nn.Linear(150, nclasses)
 
         # Spatial transformer localization-network
         self.localization = nn.Sequential(
@@ -70,7 +68,7 @@ class Net(nn.Module):
 
         return x
 
-    def forward(self, x):
+   def forward(self, x):
         bt_size = x.size(0)
 
         x = self.stn(x)
@@ -80,10 +78,8 @@ class Net(nn.Module):
         x = self.dropout(x)
         x = self.batchnorm3(F.max_pool2d(F.leaky_relu(self.conv3(x)),2))
         x = self.dropout(x)
-        x = self.batchnorm4(F.max_pool2d(F.leaky_relu(self.conv4(x)),2))
-        x = self.dropout(x)
 
-        x = x.view(-1, 1000*2*2)
+        x = x.view(-1, 250*2*2)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
