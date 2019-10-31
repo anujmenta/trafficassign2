70bb8555a286fac23d7168f7994db8b234a44684
Anuj Menta <anujmenta@gmail.com>
2019-10-31 16:44:12 -0400

    [debug] model params

--- model.py
+++ model.py
@@ -25,10 +25,10 @@ nclasses = 43 # GTSRB as 43 classes
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(3, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
+        self.conv1 = nn.Conv2d(3, 100, kernel_size=5)
+        self.conv2 = nn.Conv2d(100, 200, kernel_size=5)
         self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
+        self.fc1 = nn.Linear(3200, 50)
         self.fc2 = nn.Linear(50, nclasses)
 
         # Spatial transformer localization-network
@@ -43,7 +43,7 @@ class Net(nn.Module):
 
         # Regressor for the 3 * 2 affine matrix
         self.fc_loc = nn.Sequential(
-            nn.Linear(10 * 3 * 3, 32),
+            nn.Linear(10 * 4 * 4, 32),
             nn.ReLU(True),
             nn.Linear(32, 3 * 2)
         )
@@ -71,7 +71,7 @@ class Net(nn.Module):
         # Perform the usual forward pass
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(bt_size, -1)
+	x = x.view(-1, 3200)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
