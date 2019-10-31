55ebb086e922d8cac02d71c1ec496e330beab7dd
Anuj Menta <anujmenta@gmail.com>
2019-10-31 19:27:22 -0400

    add adam

--- main.py
+++ main.py
@@ -83,7 +83,8 @@ model = Net()
 if use_gpu:
     model.cuda()
 
-optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+#optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+optimizer = optim.Adam(model.parameters(), lr=args.lr)
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5, factor=0.5)
 
 train_loss_track = []
