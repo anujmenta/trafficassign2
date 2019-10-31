d880225981a6ab5ce56c8235084c070059db18d9
Anuj Menta <anujmenta@gmail.com>
2019-10-31 18:46:59 -0400

    [S] change model params

--- model.py
+++ model.py
@@ -31,10 +31,10 @@ class Net(nn.Module):
         self.batchnorm2 = nn.BatchNorm2d(200)
         self.conv3 = nn.Conv2d(200, 500, kernel_size=3)
         self.batchnorm3 = nn.BatchNorm2d(500)
-        self.conv4 = nn.Conv2d(500, 1000, kernel_size=3)
+        self.conv4 = nn.Conv2d(500, 1000, kernel_size=1)
         self.batchnorm4 = nn.BatchNorm2d(1000)
         self.dropout = nn.Dropout2d()
-        self.fc1 = nn.Linear(1000*2*2, 500)
+        self.fc1 = nn.Linear(1000*1*1, 500)
         self.fc2 = nn.Linear(500, nclasses)
 
         # Spatial transformer localization-network
@@ -82,8 +82,7 @@ class Net(nn.Module):
         x = self.dropout(x)
         x = self.batchnorm4(F.max_pool2d(F.leaky_relu(self.conv4(x)),2))
         x = self.dropout(x)
-
-        x = x.view(-1, 1000*2*2)
+        x = x.view(-1, 1000*1*1)
         x = F.relu(self.fc1(x))
         x = F.dropout(x, training=self.training)
         x = self.fc2(x)
