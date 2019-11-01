8ffd704e6da1242e7ab6d7e46b88f7c6f46c46a0
Anuj Menta <anujmenta@gmail.com>
2019-10-31 21:28:19 -0400

    add transforms back

--- evaluate.py
+++ evaluate.py
@@ -38,20 +38,22 @@ def pil_loader(path):
             return img.convert('RGB')
 from data import data_transform_rotate1,data_transform_rotate2,data_transform_colorjitter_brightness,data_transform_colorjitter_saturation,data_transform_colorjitter_contrast,data_transform_colorjitter_hue,data_transform_grayscale,data_transform_pad,data_transform_shear,data_transform_centercrop,data_transform_hrflip,data_transform_vrflip,data_transform_bothflip,data_transform_translate,data_transform_colorjitter_brightness_hflip,data_transform_colorjitter_saturation_hflip,data_transform_colorjitter_contrast_hflip,data_transform_colorjitter_brightness_vflip,data_transform_colorjitter_saturation_vflip,data_transform_colorjitter_contrast_vflip,data_transform_randomperspective,data_transform_vflip_rotation,data_transform_hflip_rotation
 
-transforms = [data_transform_rotate1,data_transform_rotate2,data_transform_colorjitter_brightness,data_transform_colorjitter_saturation,data_transform_colorjitter_contrast,data_transform_colorjitter_hue,data_transform_grayscale,data_transform_pad,data_transform_shear,data_transform_centercrop,data_transform_hrflip,data_transform_vrflip,data_transform_bothflip,data_transform_translate,data_transform_colorjitter_brightness_hflip,data_transform_colorjitter_saturation_hflip,data_transform_colorjitter_contrast_hflip,data_transform_colorjitter_brightness_vflip,data_transform_colorjitter_saturation_vflip,data_transform_colorjitter_contrast_vflip,data_transform_randomperspective,data_transform_vflip_rotation,data_transform_hflip_rotation]
+transforms = [data_transform_rotate1,data_transform_colorjitter_brightness,data_transform_colorjitter_saturation,data_transform_colorjitter_contrast,data_transform_colorjitter_hue,data_transform_grayscalepad,data_transform_shear,data_transform_centercrop,data_transform_hrflip,data_transform_vrflip,data_transform_translate]
 output_file = open(args.outfile, "w")
 output_file.write("Filename,ClassId\n")
 
 for f in tqdm(os.listdir(test_dir)):
     if 'ppm' in f:
-        data = data_transforms(pil_loader(test_dir + '/' + f))
-        data = data.view(1, data.size(0), data.size(1), data.size(2))
-        data = Variable(data, volatile=True)
-        output = model(data)
-        pred = output.data.max(1, keepdim=True)[1]
-
-        file_id = f[0:5]
-        output_file.write("%s,%d\n" % (file_id, pred))
+        output = torch.zeros([1, 43], dtype=torch.float32)
+        with torch.no_grad():
+            for i in range(0,len(transforms)):
+                data = transforms[i](pil_loader(test_dir + '/' + f))
+                data = data.view(1, data.size(0), data.size(1), data.size(2))
+                data = Variable(data)
+                output = output.add(model(data))
+            pred = output.data.max(1, keepdim=True)[1]
+            file_id = f[0:5]
+            output_file.write("%s,%d\n" % (file_id, pred))
 output_file.close()
 
 print("Succesfully wrote " + args.outfile + ', you can upload this file to the kaggle '
