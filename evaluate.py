6ed3ef9f14cd7049f33481b342ea60a1f6823120
Anuj Menta <anujmenta@gmail.com>
2019-10-31 21:10:16 -0400

    revert eval back to bseline

--- evaluate.py
+++ evaluate.py
@@ -44,16 +44,14 @@ output_file.write("Filename,ClassId\n")
 
 for f in tqdm(os.listdir(test_dir)):
     if 'ppm' in f:
-        output = torch.zeros([1, 43], dtype=torch.float32)
-        with torch.no_grad():
-            for i in range(0,len(transforms)):
-                data = transforms[i](pil_loader(test_dir + '/' + f))
-                data = data.view(1, data.size(0), data.size(1), data.size(2))
-                data = Variable(data)
-                output = output.add(model(data))
-            pred = output.data.max(1, keepdim=True)[1]
-            file_id = f[0:5]
-            output_file.write("%s,%d\n" % (file_id, pred))
+        data = data_transforms(pil_loader(test_dir + '/' + f))
+        data = data.view(1, data.size(0), data.size(1), data.size(2))
+        data = Variable(data, volatile=True)
+        output = model(data)
+        pred = output.data.max(1, keepdim=True)[1]
+
+        file_id = f[0:5]
+        output_file.write("%s,%d\n" % (file_id, pred))
 output_file.close()
 
 print("Succesfully wrote " + args.outfile + ', you can upload this file to the kaggle '
